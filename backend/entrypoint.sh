1d16107c660dbc07f71a1854a4e77c814759b51b
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2022-06-20 19:57:54 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,12 +11,9 @@ then
     echo "PostgreSQL started"
 fi
 
-cd /usr/src/app/
-export PYTHONPATH=/usr/src/app/;$PYTHONPATH
-
 python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
-python manage.py initadmin
 python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
