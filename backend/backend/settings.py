1abac16556be450a1723e110b63cf41f851057ca
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2022-07-11 23:52:25 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -180,7 +180,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_WHITELIST = [
-    config['cors']['host']
+    config['cors']['host'],
+    config['cors']['ip']
 ]
 
 # Default primary key field type
