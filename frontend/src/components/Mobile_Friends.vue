a7a5f85005dcdd28d8ff3e45b531ff62933a13cb
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2022-07-14 19:02:47 +0300

    Update Mobile_Friends.vue

--- frontend/src/components/Mobile_Friends.vue
+++ frontend/src/components/Mobile_Friends.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
         <div class="search-result">
-            <div class="cancel-search" @click="cancelSearch">
-                <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
-                  <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
-                </svg>
+            <div class="block-title-search">
+                <div class="cancel-search" @click="cancelSearch">
+                    <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+                      <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+                    </svg>
+                </div>
+                <p class="search-result-p">Результаты поиска</p>
+                <p class="line-search-result"></p>
             </div>
-            <p class="search-result-p">Результаты поиска</p>
-            <p class="line-search-result"></p>
             <div class="search-result-body"></div>
         </div>
         <div id="tabs">
@@ -53,7 +55,7 @@
         <div class="bottom-menu">
             <div class="div-filter">
                 <div class="div-search">
-                    <input type="text" placeholder="Кого найти?" id="filter">
+                    <input @keyup.enter.prevent="SearchUsers" type="text" placeholder="Кого найти?" id="filter">
                 </div>
                 <div class="div-search-btn" @click="SearchUsers">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
